2a898455d3d2fb3e29403804f05c618e86b5d0c7
Maksim Sadym <sadym@chromium.org>
2025-12-03 20:47:22 +0000

    Repro b/42321834

--- selenium-java/src/test/java/RegressionTest.java
+++ selenium-java/src/test/java/RegressionTest.java
@@ -19,7 +19,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -64,7 +67,20 @@ public class RegressionTest {
   }
 
   @Test
-  public void ISSUE_REPRODUCTION() {
-    // Add test reproducing the issue here.
+  public void clickableDivOverlaidOnTopOfCrossDomainIframe_shouldBeClickable() throws InterruptedException {
+    // This test reproduces https://crbug.com/chromedriver/2758
+    // The issue is that Chromedriver is unable to click a given visible element
+    // when said element is overlaid on top of a cross domain iframe.
+    driver.get("http://jackwellborn.com/playground/webdriverClickBug/clickbug.html");
+    // Move the iframe to overlap the clickable div
+    ((JavascriptExecutor) driver).executeScript("document.querySelector('body > iframe').style.top = '5px';");
+    ((JavascriptExecutor) driver).executeScript("document.querySelector('body > iframe').style.left = '0px';");
+    // Wait for the iframe to be positioned
+    Thread.sleep(1000);
+    WebElement clickableDiv = driver.findElement(By.id("clickableDiv"));
+    clickableDiv.click();
+    // The bug is that the click does not happen, so the text remains "Click Me"
+    // The test is expected to fail until the bug is fixed.
+    assertEquals("CLICKED!", clickableDiv.getText());
   }
 }
